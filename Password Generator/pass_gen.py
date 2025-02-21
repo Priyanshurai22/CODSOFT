59f8a9565cf6fb1553958c33da213bae896f3371
Priyanshu Rai <priyanshurai1920@gmail.com>
2025-02-21 16:15:24 +0530

    Add files via upload

---
+++ Password Generator/pass_gen.py
@@ -0,0 +1,22 @@
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+
+    s2 = string.ascii_uppercase
+
+    s3 = string.digits
+
+    s4 = string.punctuation
+
+    pass_len = int(input('Enter your password length: '))
+
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    print('Your password is: ')
+    print("".join(random.sample(s, pass_len)))
